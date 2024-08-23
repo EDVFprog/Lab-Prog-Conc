efecb7e5f9d8ce87185259e2cc140bee1660e9e7
Eduarda Fahr <78647206+EDVFprog@users.noreply.github.com>
2024-08-22 21:44:16 -0300

    Create gera_vet_rand._seq.c
    Código de geração de vetor randomico para  o cálculo do produto vetorial

---
+++ Lab2/gera_vet_rand._seq.c
@@ -0,0 +1,81 @@
+/*
+/* Disciplina: Computacao Concorrente */
+/* Aluna.: Eduarda Varela Fahr */
+/* Lab2:  */
+/* Codigo:Gera os vetores que serão utilizados para o produto vetorial
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX 1000 //valor maximo de um elemento do vetor
+
+#define TEXTO 
+float* criaVetor(long int n){
+float elem;
+int fator=1; 
+float* vetor = (float*)malloc(n * sizeof(float));
+   //preenche o vetor com valores float aleatorios
+   for(long int i=0; i<n; i++) {
+        elem = (rand() % MAX)/3.0 * fator;
+        vetor[i] = elem;
+        
+        fator*=-1;
+       }
+    return vetor;
+}
+
+int main(int argc, char*argv[]) {
+ 
+   long int n; //qtde de elementos do vetor que serao gerados
+   float elem; //valor gerado para incluir no vetor
+   FILE * descritorArquivo; //descritor do arquivo de saida
+   size_t ret; //retorno da funcao de escrita no arquivo de saida
+
+   //recebe os argumentos de entrada
+   if(argc < 3) {
+      fprintf(stderr, "Digite: %s <dimensao> <arquivo saida>\n", argv[0]);
+      return 1;
+   }
+   n = atoi(argv[1]);
+  float *vetor=criaVetor(n);// criando o vetor
+  float *vetorb=criaVetor(n);// cria o vetor b
+
+
+   //imprimir na saida padrao o vetor gerado
+   #ifdef TEXTO
+   fprintf(stdout, "%ld\n", n);
+   for(long int i=0; i<n; i++) {
+      fprintf(stdout, "%f ",vetor[i]);
+   }
+   fprintf(stdout, "\n");
+   for(long int i=0; i<n; i++) {
+      fprintf(stdout, "%f ",vetorb[i]);
+   }
+   fprintf(stdout, "\n");
+ #endif
+   //escreve o vetor no arquivo
+   //abre o arquivo para escrita binaria
+   descritorArquivo = fopen(argv[2], "wb");
+   if(!descritorArquivo) {
+      fprintf(stderr, "Erro de abertura do arquivo\n");
+      return 3;
+   }
+   //escreve a dimensao
+   ret = fwrite(&n, sizeof(long int), 1, descritorArquivo);
+   //escreve os elementos do vetor
+   ret = fwrite(vetor, sizeof(float), n, descritorArquivo);
+   // escrevendo os elementos do vetorb
+   ret =fwrite(vetorb,sizeof(float),n,descritorArquivo);
+   if(ret < n) {
+      fprintf(stderr, "Erro de escrita no  arquivo\n");
+      return 4;
+   }
+
+   //finaliza o uso das variaveis
+   fclose(descritorArquivo);
+   free(vetor);
+  free(vetorb);
+   return 0;
+} 
