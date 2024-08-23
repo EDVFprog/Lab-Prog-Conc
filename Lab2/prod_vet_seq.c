04376e92b51f1b686df5517bce6b9a655c9df944
Eduarda Fahr <78647206+EDVFprog@users.noreply.github.com>
2024-08-23 11:12:36 -0300

    Update and rename gera_vet_rand._seq.c to prod_vet_seq.c
    Programa sequencial que gera os vetores e depois calcula o produto vetorial entre eles. Escrevendo seus valores em um arquivo binário

--- Lab2/prod_vet_seq.c
+++ Lab2/prod_vet_seq.c
@@ -10,20 +10,30 @@
 #include <time.h>
 
 #define MAX 1000 //valor maximo de um elemento do vetor
+#define TEXTO // imprime os valores gerados do vetor
 
-#define TEXTO 
+
+// calcula o produto vetorial
+double prodVet(float *vet1,float *vet2,long int N){
+  double soma=0.0;
+  for(int i=0; i<N;i++){
+    soma+=vet1[i]*vet2[i];
+  }
+  return soma;
+}
+// cria o vetor
 float* criaVetor(long int n){
-float elem;
-int fator=1; 
-float* vetor = (float*)malloc(n * sizeof(float));
-   //preenche o vetor com valores float aleatorios
-   for(long int i=0; i<n; i++) {
+  float elem;
+  int fator=1; 
+  float* vetor = (float*)malloc(n * sizeof(float));
+  //preenche o vetor com valores float aleatorios
+  for(long int i=0; i<n; i++) {
         elem = (rand() % MAX)/3.0 * fator;
         vetor[i] = elem;
         
         fator*=-1;
-       }
-    return vetor;
+  }
+  return vetor;
 }
 
 int main(int argc, char*argv[]) {
@@ -37,10 +47,10 @@ int main(int argc, char*argv[]) {
    if(argc < 3) {
       fprintf(stderr, "Digite: %s <dimensao> <arquivo saida>\n", argv[0]);
       return 1;
-   }
+    }
    n = atoi(argv[1]);
-  float *vetor=criaVetor(n);// criando o vetor
-  float *vetorb=criaVetor(n);// cria o vetor b
+   float *vetor=criaVetor(n);// criando o vetor
+   float *vetorb=criaVetor(n);// cria o vetor b
 
 
    //imprimir na saida padrao o vetor gerado
@@ -55,7 +65,7 @@ int main(int argc, char*argv[]) {
    }
    fprintf(stdout, "\n");
  #endif
-   //escreve o vetor no arquivo
+   
    //abre o arquivo para escrita binaria
    descritorArquivo = fopen(argv[2], "wb");
    if(!descritorArquivo) {
@@ -68,14 +78,19 @@ int main(int argc, char*argv[]) {
    ret = fwrite(vetor, sizeof(float), n, descritorArquivo);
    // escrevendo os elementos do vetorb
    ret =fwrite(vetorb,sizeof(float),n,descritorArquivo);
+   
    if(ret < n) {
       fprintf(stderr, "Erro de escrita no  arquivo\n");
       return 4;
    }
-
-   //finaliza o uso das variaveis
+   double prod=prodVet(vetor,vetorb,n);
+   // Teste para ver se produto vetorial  está correto 
+   printf("%f",prod);
+   // escrevendo o resultado do produto vetorial no arquivo binario
+   ret =fwrite(&prod,sizeof(double),1,descritorArquivo);
+   //finaliza o uso das variaveis e fecha o arquivo
    fclose(descritorArquivo);
    free(vetor);
-  free(vetorb);
+   free(vetorb);
    return 0;
 } 
