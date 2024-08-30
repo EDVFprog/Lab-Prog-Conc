a4eac67f00ccbecfd22edf24209780b911244b7d
Eduarda Fahr <78647206+EDVFprog@users.noreply.github.com>
2024-08-30 20:39:44 -0300

    Update matmult_seq.c
    Código sequencial atualizado

--- Lab3/matmult_seq.c
+++ Lab3/matmult_seq.c
@@ -2,29 +2,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<pthread.h>
+#include "timer.h"
 
-
-
+// Função que calcula a multiplicação de 2 matrizes
 float* multMat(int linhasA, int colunasA, float *matrizA, int linhasB, int colunasB,float* matrizB){
-  int tamS =colunasA*linhasB;
-     float * result = (float*) malloc(sizeof(float) * tamS);
+   int tamS= colunasA*linhasB;// tamanho  dos elementos da matriz de saída
+
+   float * res = (float*) malloc(sizeof(float) * tamS);// matriz de saída
+   //trata caso a multiplicação não seja possível
      if(colunasA!= linhasB){
-        printf("As matrizes não podem ser multiplicadas erro de dimensão.");
-        }
+        /*printf("As matrizes não podem ser multiplicadas erro de dimensão.");*/
+        return NULL;
+      }
      else{
-
-     printf("Resultante \n");
-      
-
-     for(int i=0; i<linhasA;i++){
+//calculo da multiplicação
+      for(int i=0; i<linhasA;i++){
    
         for (int j=0;j<colunasB;j++){
-          float soma=0.0;
+          
          for (int k =0;k<colunasA; k++){
         
-            
-           result[i*colunasB+j]+=  matrizA[i*colunasA + k] * matrizB[k*colunasB+j];
-            
+            res[i*colunasB+j]+=  matrizA[i*colunasA + k] * matrizB[k*colunasB+j];
             
             }
            
@@ -35,10 +33,10 @@ float* multMat(int linhasA, int colunasA, float *matrizA, int linhasB, int colun
  }
     
      
-return result;
+return res;
 
 }
-
+// main
 int main(int argc, char* argv[]) {
     float *matrizA; //matriz que será carregada do arquivo matriz A
    int linhasA, colunasA; //dimensoes da matriz A
@@ -46,10 +44,11 @@ int main(int argc, char* argv[]) {
     int linhasB, colunasB; //dimensoes da matriz B
    long long int tamA,tamB; //qtde de elementos na matriz A e B
    FILE * arqA, *arqB,*arqS; //descritor do arquivos de entrada e arquivo de saída
-   int tam= colunasA*linhasB;
    size_t retA,retB,ret; //retorno da funcao de escrita no arquivo de saida
-   
+    int tamS= colunasA*linhasB;// tamanho  dos elementos da matriz de saída
+    double inicio, fim, delta;// monitoramento de tempo
    //recebe os argumentos de entrada
+   GET_TIME(inicio);
    if(argc < 3) {
       fprintf(stderr, "Digite: %s <arquivos de entrada> e <arquivo de saída> \n", argv[0]);
       return 1;
@@ -96,12 +95,12 @@ int main(int argc, char* argv[]) {
       return 4;
    }
    
-   //imprime a matriz na saida padrao
+   /*//imprime a matriz na saida padrao
    for(int i=0; i<linhasA; i++) { 
       for(int j=0; j<colunasA; j++)
         fprintf(stdout, "%.6f ", matrizA[i*colunasA+j]);
       fprintf(stdout, "\n");
-   }
+   }*/
   
    //le as dimensoes da matriz B
    retB = fread(&linhasB, sizeof(int), 1, arqB);
@@ -123,29 +122,38 @@ int main(int argc, char* argv[]) {
       return 3;
    }
 
-   //carrega a matriz de elementos do tipo float do arqA
+   //carrega a matriz de elementos do tipo float do arqB
    retB= fread(matrizB, sizeof(float), tamB, arqB);
    if(retB < tamB) {
       fprintf(stderr, "Erro de leitura dos elementos da matriz\n");
       return 4;
    }
       printf("\n");
- 
+ GET_TIME(fim);
+ delta = fim - inicio;
+   printf("Tempo inicializacao:%lf\n", delta);
+ /*
  //imprime a matriz de entrada B
    for(int i=0; i<linhasB; i++) { 
       for(int j=0; j<colunasB; j++)
         fprintf(stdout, "%.6f ", matrizB[i*colunasB+j]);
       fprintf(stdout, "\n");
-   }
-
+   }*/
+   // Atribui M a matriz de saída 
+   //multiplicacao da matriz pelo vetor
+   GET_TIME(inicio);
    float *M =multMat( linhasA,  colunasA,  matrizA,  linhasB,  colunasB, matrizB);
    //imprime a matriz na saida padrao
-    for(int i=0; i<linhasA; i++) { 
+   /* for(int i=0; i<linhasA; i++) { 
       for(int j=0; j<colunasB; j++)
         fprintf(stdout, "%.6f ", M[i*colunasB+j]);
       fprintf(stdout, "\n");
-   }
+   }*/
    // escreve a matriz de saída e suas dimensões no arquivo binário de saída
+ GET_TIME(fim)   
+   delta = fim - inicio;
+   printf("Tempo multiplicacao: %lf\n", delta);
+    GET_TIME(inicio);
    //abre o arquivo para escrita binaria
    arqS = fopen(argv[3], "wb");
    if(!arqS) {
@@ -153,10 +161,10 @@ int main(int argc, char* argv[]) {
       return 3;
    }
     //escreve numero de linhas e de colunas
-    ret = fwrite(&linhasA, sizeof(int), 1, arqS);
+   ret = fwrite(&linhasA, sizeof(int), 1, arqS);
    ret = fwrite(&colunasB, sizeof(int), 1, arqS);
    //escreve os elementos da matriz
-   ret = fwrite(M, sizeof(float), tam, arqS);
+   ret = fwrite(M, sizeof(float), tamS, arqS);
     //finaliza o uso das variaveis
    fclose(arqA);
    free(matrizA);
@@ -164,5 +172,8 @@ int main(int argc, char* argv[]) {
    free(matrizB);
    fclose(arqS);
    free(M);
+   GET_TIME(fim)   
+   delta = fim - inicio;
+   printf("Tempo finalizacao:%lf\n", delta);
    return 0;
 }
